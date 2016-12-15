c3f0812072faedbab407b00028e41a441d5cfe9b
itlijunjie <itlijunjie@gmail.com>
2016-12-15 17:21:25 +0800

    modify 初始化项目

---
+++ SocialSDK/SocialSDK.h
@@ -0,0 +1,19 @@
+//
+//  SocialSDK.h
+//  SocialSDK
+//
+//  Created by lijunjie on 15/12/2016.
+//  Copyright © 2016 GagSquad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocialSDK.
+FOUNDATION_EXPORT double SocialSDKVersionNumber;
+
+//! Project version string for SocialSDK.
+FOUNDATION_EXPORT const unsigned char SocialSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialSDK/PublicHeader.h>
+
+
---
+++ SocialSDKDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialSDKDemo
+//
+//  Created by lijunjie on 15/12/2016.
+//  Copyright © 2016 GagSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
