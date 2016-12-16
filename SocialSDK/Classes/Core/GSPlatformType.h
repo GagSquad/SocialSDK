f98481b448ee48f058550f253d17418dfd84b6fd
itlijunjie <itlijunjie@gmail.com>
2016-12-16 14:13:20 +0800

    modify 添加枚举值

---
+++ SocialSDK/Classes/Core/GSPlatformType.h
@@ -0,0 +1,16 @@
+//
+//  GSPlatformType.h
+//  SocialSDKDemo
+//
+//  Created by lijunjie on 16/12/2016.
+//  Copyright © 2016 GagSquad. All rights reserved.
+//
+
+#ifndef GSPlatformType_h
+#define GSPlatformType_h
+
+typedef enum : NSUInteger {
+    GSPlatformTypeSina
+} GSPlatformType;
+
+#endif /* GSPlatformType_h */
