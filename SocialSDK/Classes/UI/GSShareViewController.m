fc436bcdb593072a3ec454a9fff64bf929b10518
itlijunjie <itlijunjie@gmail.com>
2016-12-17 18:57:12 +0800

    modify 修改选择UI

--- SocialSDK/Classes/UI/GSShareViewController.m
+++ SocialSDK/Classes/UI/GSShareViewController.m
@@ -31,11 +31,37 @@
         self.frame = [UIScreen mainScreen].bounds;
         self.userInteractionEnabled = YES;
         _channels = channels;
+        [self createView];
     }
     return self;
 }
 
+- (void)createView
+{
+    CGFloat height = 100.f;
+    
+    CGSize size = self.frame.size;
+    UIView *shareBGView = [[UIView alloc] initWithFrame:CGRectMake(0, size.height - height, size.width, height)];
+    shareBGView.backgroundColor = [UIColor redColor];
+    [self addSubview:shareBGView];
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, size.width, 40)];
+    headerView.backgroundColor = [UIColor brownColor];
+    UILabel *title = [[UILabel alloc] initWithFrame:headerView.bounds];
+    title.textAlignment = NSTextAlignmentCenter;
+    title.text = @"选择分享平台";
+    [headerView addSubview:title];
+    
+    [shareBGView addSubview:headerView];
+    
+}
+
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self remove];
+}
+
+- (void)remove
 {
     [self removeFromSuperview];
 }
