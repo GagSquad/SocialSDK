dd7f097158d2e591e6253c8174ecc481512cd01c
lvjialin <996691799@qq.com>
2016-12-17 14:27:16 +0800

    modify 更新 sina分享网页版授权

--- SocialSDKDemo/AppDelegate.swift
+++ SocialSDKDemo/AppDelegate.swift
@@ -44,14 +44,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return GSShareManager.share().handleOpen(url)
-    }
     
     func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
         return GSShareManager.share().handleOpen(url)
     }
     
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return GSShareManager.share().handleOpen(url)
+    }
     
 }
 
