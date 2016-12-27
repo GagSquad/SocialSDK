a1c737120eab0425cfc9d051d8ee77db4f9898bd
itlijunjie <itlijunjie@gmail.com>
2016-12-27 09:58:13 +0800

    modify 修改登录支持

---
+++ SocialSDK/Classes/Login/GSLoginChannelType.h
@@ -0,0 +1,19 @@
+//
+//  GSLoginChannelType.h
+//  SocialSDKDemo
+//
+//  Created by lijunjie on 23/12/2016.
+//  Copyright © 2016 GagSquad. All rights reserved.
+//
+
+#ifndef GSLoginChannelType_h
+#define GSLoginChannelType_h
+
+typedef enum : NSUInteger {
+    GSLoginChannelTypeNone,
+    GSLoginChannelTypeSina,
+    GSLoginChannelTypeQQ,
+    GSLoginChannelTypeWeChat
+} GSLoginChannelType;
+
+#endif /* GSLoginChannelType_h */
