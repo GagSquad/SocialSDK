20eb2ede6dc5e3c5b3cba1aaaf9c89169ec41827
itlijunjie <itlijunjie@gmail.com>
2016-12-27 10:51:14 +0800

    modify 添加基类

---
+++ SocialSDK/Classes/Login/GSLoginProtocol.h
@@ -0,0 +1,13 @@
+//
+//  GSLoginProtocol.h
+//  SocialSDKDemo
+//
+//  Created by lijunjie on 27/12/2016.
+//  Copyright © 2016 GagSquad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GSLoginProtocol <NSObject>
+
+@end
