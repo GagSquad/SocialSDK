e71f17dcfbb84b355b7df136715d6809284e9078
itlijunjie <itlijunjie@gmail.com>
2017-08-22 11:02:57 +0800

    modify 修改第三方配置文件，需要替换成自己的

---
+++ SocialSDKDemo/CUThirdPartyConstant.h
@@ -0,0 +1,30 @@
+//
+//  CUThirdPartyConstant.h
+//  CommonUtil
+//
+//  Created by MrLv on 15/9/14.
+//  Copyright (c) 2015年 GagSquad. All rights reserved.
+//
+
+#ifndef CommonUtil_CUThirdPartyConstant_h
+#define CommonUtil_CUThirdPartyConstant_h
+
+#define WeiXinKey @"自己的"
+#define WeiXinSecret @"自己的"
+#define WeiXinCallBack @"自己的"
+
+#define SinaWeiBoKey @"自己的"
+#define SinaWeiBoSecret @"自己的"
+#define SinaWeiBoCallBack @"自己的"
+#define SinaWeiBoCancelCallBack @"自己的"
+
+#define QQAppId @"自己的"
+#define QQAppKey @"自己的"
+#define QQCallBack @"自己的"
+
+//后面支付用
+//#define kAliPayResult @"自己的"
+//#define kWeixinPayResult @"自己的"
+//#define kAlipayScheme @"自己的"
+
+#endif
